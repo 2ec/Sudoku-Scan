3c875f4a3c96bcfe0b8b7aa01468e5027aa66732
Emil Strømsvåg <emilchri@hotmail.com>
2020-10-04 15:15:58 +0200

    Test Commit

---
+++ OpenCV.m
@@ -0,0 +1,13 @@
+//
+//  OpenCV.m
+//  Sudoku Scan
+//
+//  Created by Emil Christopher Gjøstøl Strømsvåg on 30/07/2020.
+//  Copyright © 2020 Emil Christopher Gjøstøl Strømsvåg. All rights reserved.
+//
+
+#import "OpenCV.h"
+
+@implementation OpenCV
+
+@end
