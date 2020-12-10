7f57188f274ce93c8d8186728d402f713af809b6
2ec <emilchri@hotmail.com>
2020-12-10 14:44:48 +0100

    First commit

--- Sudoku Scan/ViewController.swift
+++ Sudoku Scan/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var grid: UICollectionView!
     @IBOutlet weak var textLabel: UILabel!
     // Test comment
+    // NEw
     
     
     //var teller:Int = 0
